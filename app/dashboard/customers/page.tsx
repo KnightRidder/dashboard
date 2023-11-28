5a7de4196434e52f675a6d8d259f40f91988c174
KnightRidder <81036914+KnightRidder@users.noreply.github.com>
2023-11-29 01:27:27 +0500

    Completed project

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,22 @@
-export default function Page() {
-  return <p>Customer Page</p>;
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import { TableRowSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <Suspense fallback={<TableRowSkeleton />}>
+      <CustomersTable customers={customers} />
+    </Suspense>
+  );
 }
